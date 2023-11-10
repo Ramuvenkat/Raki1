58b730bd17ce447ff5bffdb16601804327f33c4d
venka <venkateswarluvcs@gmail.com>
2023-11-10 14:11:31 +0530

    demo commit

---
+++ Raki/src/raki1/Venkat.java
@@ -0,0 +1,17 @@
+package raki1;
+class Ramu{
+	public void m1() {
+		System.out.println("Parent class");
+	}
+}
+
+public class Venkat extends Ramu {
+
+	public static void main(String[] args) {
+		Venkat v=new Venkat();
+		v.m1();
+
+	}
+
+}
+
